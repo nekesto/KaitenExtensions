f3bb7e10aaedf815e618c74dda880ee94e27fa92
David Avendasora <avendasorada@me.com>
2012-01-06 12:57:00 +0800

    Initial Commit

---
+++ KaitenExtensions/KaitenExtensions.h
@@ -0,0 +1,13 @@
+//
+//  KaitenExtensions.h
+//  KaitenExtensions
+//
+//  Created by David Avendasora on 1/6/12.
+//  Copyright (c) 2012 Kaiten Enterprise Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KaitenExtensions : NSObject
+
+@end
---
+++ KaitenExtensions/KaitenExtensions.m
@@ -0,0 +1,23 @@
+//
+//  KaitenExtensions.m
+//  KaitenExtensions
+//
+//  Created by David Avendasora on 1/6/12.
+//  Copyright (c) 2012 Kaiten Enterprise Systems. All rights reserved.
+//
+
+#import "KaitenExtensions.h"
+
+@implementation KaitenExtensions
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
