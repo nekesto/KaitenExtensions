e9a072861c9b835ce4e1dc05f9b57b913f9c5808
David Avendasora <avendasorada@me.com>
2012-05-29 15:33:34 +0800

    Get multi-platform frameworks working better

--- Common/KaitenExtensions.h
+++ Common/KaitenExtensions.h
@@ -1,13 +1,11 @@
 //
-//  KaitenExtensionsIOS.h
-//  KaitenExtensionsIOS
+//  KaitenExtensions.h
+//  KaitenExtensions
 //
 //  Created by David Avendasora on 5/28/12.
 //  Copyright (c) 2012 Kaiten Enterprise Systems. All rights reserved.
 //
 
-#import <CoreData/CoreData.h>
-#import <Foundation/Foundation.h>
 #import "NSManagedObjectContext-EasyFetch.h"
 #import "NSArray+KaitenExtensions.h"
 #import "NSPredicate+KaitenExtensions.h"
--- KaitenExtensionsMOS/KaitenExtensionsMOS.h
+++ KaitenExtensionsMOS/KaitenExtensionsMOS.h
@@ -6,13 +6,6 @@
 //  Copyright (c) 2012 Kaiten Enterprise Systems. All rights reserved.
 //
 
-#import <CoreData/CoreData.h>
-#import <Foundation/Foundation.h>
-#import "NSArray+KaitenExtensions.h"
-#import "NSPredicate+KaitenExtensions.h"
-#import "KXKey.h"
-#import "NSManagedObjectContext-EasyFetch.h"
-
 @interface KaitenExtensionsMOS : NSObject
 
 @end
