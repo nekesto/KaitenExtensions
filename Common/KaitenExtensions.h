bdf02c017ec1b13390e60968975d125fdb70a694
David Avendasora <avendasorada@me.com>
2012-06-09 02:43:47 +0800

    Add some groups to the project, plus improve the public header file.

--- Common/KaitenExtensions.h
+++ Common/KaitenExtensions.h
@@ -6,14 +6,14 @@
 //  Copyright (c) 2012 Kaiten Enterprise Systems. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 #import "NSManagedObjectContext-EasyFetch.h"
 #import "NSArray+KaitenExtensions.h"
 #import "NSPredicate+KaitenExtensions.h"
 #import "KXKey.h"
 #import "NSDate+KaitenExtensions.h"
 #import "NSFileManager+DirectoryLocations.h"
-
-
 
 @interface KaitenExtensions : NSObject
 
